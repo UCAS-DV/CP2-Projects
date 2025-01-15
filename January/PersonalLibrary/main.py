7e5402340a43d7331adccb039440914a4aa968d4
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-01-15 10:50:36 -0700

    Parentheses will be the end of me.

---
+++ January/PersonalLibrary/main.py
@@ -0,0 +1,46 @@
+# Darius Vaiaoga - P2 - Personal Library Program
+
+library = []
+
+def showLibrary():
+    num = 1
+    for game in library:
+        print(f'{num}. {game["name"]} - {game["genre"]} - {game["developer"]}')
+        num += 1
+
+def createEntry():
+    name = input("What is the name of your game? ")
+    genre = input("What genre is your game? ").capitalize()
+    developer = input("What studio or person developed or published the game? ")
+
+    library.append({"name": name, "genre": genre, "developer": developer})
+
+def removeEntry():
+    showLibrary()
+    gameToRemove = input("Which game do you want to remove? ")
+    
+    for game in library:
+        if game['name'].lower() == gameToRemove.lower():
+            library.remove(game)
+            print(f"{game['name']} has been removed from your library.")
+            break
+    else:
+        print(f'Could not find "{gameToRemove}" in your library. Please verify if you spelt the name correctly.')
+    
+
+def main():
+    username = input("Welcome to your personal game library. Enter a username: ")
+    while True:
+        print(f"{username}'s Library")
+
+        match input("What would you like to do? \n1. Go to Library \n2. Add to Library \n3. Remove from Library \n4. Search in Library \n5. Exit \n"):
+            case "1":
+                showLibrary()
+            case "2":
+                createEntry()
+            case "3":
+                removeEntry()
+
+        input("Press Enter to continue.")
+                
+main()
