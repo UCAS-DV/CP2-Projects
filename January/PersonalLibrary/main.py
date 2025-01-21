0e73cb0b7b70dfd2b6266d8f18c75cf2d73afbf4
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-01-21 21:41:17 +0000

    yay!

--- January/PersonalLibrary/main.py
+++ January/PersonalLibrary/main.py
@@ -2,30 +2,63 @@
 
 library = []
 
+genres = (
+    "Action-Adventure",
+    "Fighting",
+    "FPS",
+    "Horror",
+    "Party", 
+    "Platformer",
+    "Puzzle",
+    "Roguelike",
+    "RPG",
+    "RTS",
+    "Sandbox",
+    "Simulator",
+    "Survival",
+)
+
 def showLibrary():
     num = 1
+    print("Your Library:")
     for game in library:
         print(f'{num}. {game["name"]} - {game["genre"]} - {game["developer"]}')
         num += 1
 
 def createEntry():
     name = input("What is the name of your game? ")
-    genre = input("What genre is your game? ").capitalize()
+    
+    num = 1
+    for possible_genre in genres:
+        print(f"{num}. {possible_genre}")
+        num += 1
+
+    try:
+        genre = genres[int(input("Which genre is your game? (Input Number) ")) - 1]
+    except:
+        input("Invalid Input")
+        return None
+
     developer = input("What studio or person developed or published the game? ")
 
     library.append({"name": name, "genre": genre, "developer": developer})
 
 def removeEntry():
     showLibrary()
-    gameToRemove = input("Which game do you want to remove? ")
+    game_to_remove = input("Which game do you want to remove? ")
     
     for game in library:
-        if game['name'].lower() == gameToRemove.lower():
+        if game['name'].lower() == game_to_remove.lower():
             library.remove(game)
             print(f"{game['name']} has been removed from your library.")
             break
     else:
-        print(f'Could not find "{gameToRemove}" in your library. Please verify if you spelt the name correctly.')
+        print(f'Could not find "{game_to_remove}" in your library. Please verify if you spelt the name correctly.')
+
+def search():
+    query = input("Enter search query")
+    search_list = []
+    for letter in query:
     
 
 def main():
@@ -40,6 +73,10 @@ def main():
                 createEntry()
             case "3":
                 removeEntry()
+            case "4":
+                search()
+            case "5":
+                break
 
         input("Press Enter to continue.")
                 
