c87b10e79e23524d64e246ef1227a666b3e2e9d4
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-03-03 22:30:26 +0000

    Fixed stuff

--- February/ToDoList/main.py
+++ February/ToDoList/main.py
@@ -15,6 +15,32 @@ def add_item(new_item):
     with open('February/ToDoList/to_do.txt', 'a') as to_do_file:
         to_do_file.write(f'[-]{new_item},')
 
+# Removes item from txt file
+def remove_item(item_to_remove, checked_off):
+
+    format_item_to_remove = ''
+
+    # Formats the item_to_remove to prep it for removal
+    if not checked_off:
+        format_item_to_remove = f'[-]{item_to_remove}'
+    else:
+        format_item_to_remove = f'[x]{item_to_remove}'
+
+    to_do_list = read_list()
+
+    # Checks if the item to remove is in the to_do_list
+    if format_item_to_remove in to_do_list:
+
+        to_do_list.remove(format_item_to_remove)
+
+        # Re adds all of the items that weren't removeed
+        with open('February/ToDoList/to_do.txt', 'w') as to_do_file:
+            for item in to_do_list:
+                to_do_file.write(f'{item},')
+    else:
+        input("Couldn't find item to remove (Press enter to continue)")
+
+# Marks items as complete    
 def mark_item(item_to_mark, mark_complete):
 
     to_do_list = read_list()
@@ -57,11 +83,11 @@ def print_list():
 
 def main():
 
-    input('-~-~-~-~-~- To-Do List -~-~-~-~-~-')
+    print('-~-~-~-~-~- To-Do List -~-~-~-~-~-')
 
     while True:
 
-        match input('What do you want to do? \n1. View List \n2. Add Item \n3. Mark Item \n4. Exit \nChoose a number: '):
+        match input('What do you want to do? \n1. View List \n2. Add Item \n3. Remove Item \n4. Mark Item \n5. Exit \nChoose a number: '):
             
             # View List
             case '1':
@@ -71,25 +97,34 @@ def main():
             case '2':
                 add_item(input('What item do you want to add to your To-Do List? '))
             
-            # Mark Item
             case '3':
+                print_list()
+
+                match input('Is this item checked off (Has [x] next to it)? \n1. Yes \n2. No \nChoose a number: '):
+                    case '1':
+                        print_list()
+                        remove_item(input('What item do you wish to remove? '), True)
+                    case '2':
+                        print_list()
+                        remove_item(input('What item do you wish to remove? '), False)
+
+            # Mark Item
+            case '4':
 
                 match input('Do you want to check off or uncheck off an item? \n1. Check off \n2. Uncheck off \nChoose a number: '):
                     
                     # Check Off
                     case '1':
                         print_list()
-
                         mark_item(input('What item do you want to check off? (Answer is case sensitive): '), True)
 
                     # Uncheck Off
                     case '2':
                         print_list()
-
                         mark_item(input('What item do you want to check off? (Answer is case sensitive): '), False)
 
             # Exit
-            case '4':
+            case '5':
                 break
 
             # Invalid Input
