24e1929f897ee9afb127275043fba0073d3cd686
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-03-28 10:23:07 -0600

    Reformatted

--- March/CoinProblemSolver/main.py
+++ March/CoinProblemSolver/main.py
@@ -1,53 +1,5 @@
 import countries
-
-def solve():
-
-    while True:
-        i = 1
-        for country in countries.country_dems:
-            print(f'{i}. {country}')
-            i += 1
-
-        try:
-            country_index = input('Input abbrieviation of country you want to solve for: ')
-
-            country = countries.country_dems[country_index.upper()]
-            
-            target = float(input('Input target amount: '))
-
-        # Ensures that the user inputs the right inputs
-        except:
-            print('Invalid Input')
-            continue
-        
-        # Prevents the target from being less than or equal to zero
-        if target <= 0:
-            print('Invalid Input')
-            continue
-
-        denomination_amounts = []
-
-        for denomination in country:
-
-            # Adds denomination and it's amount to denomination_amounts
-            if target // country[denomination] > 0:
-                denomination_amounts.append(denomination)
-                denomination_amounts.append(int(target // country[denomination]))
-
-            target -= (target // country[denomination]) * country[denomination]
-            target = round(target, 2)
-
-        # Formats the answer
-        final_output = ''
-        for amount in denomination_amounts:    
-
-            if type(amount) == str:
-                final_output += f'{amount}: '
-
-            else:
-                final_output += f'{amount}\n'
-
-        return final_output
+import solve
 
 def main():
     while True:
@@ -58,7 +10,7 @@ def main():
                 input('The Coin Change Problem is a classic alogrithmic problem.\nThe problem is if given a target amount of currency, what is the minimum amount of coins and banknotes necessary to reach that target amount \nIn this program, you will enter the target currency and the type of currency and then this program will solve the problem. \n(Enter to Continue)')
             # Solve
             case '2':
-                print(solve())
+                print(solve.solve())
             # Exit
             case '3':
                 print('Exiting Program...')
---
+++ March/CoinProblemSolver/solve.py
@@ -0,0 +1,58 @@
+import countries
+
+def solve():
+
+    def solve_menu():
+        i = 1
+        for country in countries.country_dems:
+            print(f'{i}. {country}')
+            i += 1
+
+        try:
+            country_index = input('Input abbrieviation of country you want to solve for: ')
+
+            country = countries.country_dems[country_index.upper()]
+            
+            target = float(input('Input target amount: '))
+
+        # Ensures that the user inputs the right inputs
+        except:
+            print('Invalid Input')
+            return -1, -1
+        
+        # Prevents the target from being less than or equal to zero
+        if target <= 0:
+            print('Invalid Input')
+            return -1, -1
+        
+        return country, target
+
+    while True:
+
+        user_country, user_target = solve_menu()
+        if user_country == -1 and user_target == -1:
+            continue
+        
+        denomination_amounts = []
+
+        for denomination in user_country:
+
+            # Adds denomination and it's amount to denomination_amounts
+            if user_target // user_country[denomination] > 0:
+                denomination_amounts.append(denomination)
+                denomination_amounts.append(int(user_target // user_country[denomination]))
+
+            user_target -= (user_target // user_country[denomination]) * user_country[denomination]
+            user_target = round(user_target, 2)
+
+        # Formats the answer
+        final_output = ''
+        for amount in denomination_amounts:    
+
+            if type(amount) == str:
+                final_output += f'{amount}: '
+
+            else:
+                final_output += f'{amount}\n'
+
+        return final_output
